fbc67573a68d1d2d19c635d1d9a6b284d1a9cd82
yumyeonghan <mgtmh991013@gmail.com>
2022-11-30 17:49:49 +0900

    test: [Game Test], 볼 기능 테스트

--- src/test/java/baseball/GameTest.java
+++ src/test/java/baseball/GameTest.java
@@ -1,18 +1,30 @@
 package baseball;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 class GameTest {
+
+    private List<Integer> computerNumber;
+    private List<Integer> userNumber;
+
     @Test
     void 스트라이크() {
-        List<Integer> computerNumber = new ArrayList<>(Arrays.asList(1,2,3));
-        List<Integer> userNumber = new ArrayList<>(Arrays.asList(1,2,3));
-        Assertions.assertThat(Game.strike(computerNumber, userNumber)).isEqualTo(3);
+        computerNumber = new ArrayList<>(Arrays.asList(1,2,3));
+        userNumber = new ArrayList<>(Arrays.asList(1,2,3));
+        assertThat(Game.strike(computerNumber, userNumber)).isEqualTo(3);
+    }
+
+    @Test
+    void 볼() {
+        computerNumber = new ArrayList<>(Arrays.asList(3,1,2));
+        userNumber = new ArrayList<>(Arrays.asList(1,2,3));
+        assertThat(Game.ball(computerNumber, userNumber)).isEqualTo(3);
     }
 
 }
