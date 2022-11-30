0e45516ad116e24d8b28aecad62a1a058f64f07e
yumyeonghan <mgtmh991013@gmail.com>
2022-11-30 17:00:52 +0900

    test:  [Game Test],스트라이크 기능 테스트

---
+++ src/test/java/baseball/GameTest.java
@@ -0,0 +1,19 @@
+package baseball;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class GameTest {
+    @Test
+    void 스트라이크() {
+        List<Integer> computerNumber = new ArrayList<>(Arrays.asList(1,2,3));
+        List<Integer> userNumber = new ArrayList<>(Arrays.asList(1,2,3));
+        Assertions.assertThat(Game.strike(computerNumber, userNumber)).isTrue();
+    }
+
+}
+
