d90b37cc0460a87be9e909b1a48353d7e63c7721
yumyeonghan <mgtmh991013@gmail.com>
2022-11-30 17:25:44 +0900

    fix:  [Game Test],스트라이크 테스트 수정

--- src/test/java/baseball/GameTest.java
+++ src/test/java/baseball/GameTest.java
@@ -12,7 +12,7 @@ class GameTest {
     void 스트라이크() {
         List<Integer> computerNumber = new ArrayList<>(Arrays.asList(1,2,3));
         List<Integer> userNumber = new ArrayList<>(Arrays.asList(1,2,3));
-        Assertions.assertThat(Game.strike(computerNumber, userNumber)).isTrue();
+        Assertions.assertThat(Game.strike(computerNumber, userNumber)).isEqualTo(3);
     }
 
 }
