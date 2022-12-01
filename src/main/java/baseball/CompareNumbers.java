44723fb086022cad17b5f1f49d42a6d498b12dc3
yumyeonghan <mgtmh991013@gmail.com>
2022-12-01 09:31:37 +0900

    refactor: 중복 코드 제거
    CompareNumbers 클래스를 만들어 중복되는 매개변수, computerNumber와 userNumber를 묶음

---
+++ src/main/java/baseball/CompareNumbers.java
@@ -0,0 +1,25 @@
+package baseball;
+
+import java.util.List;
+
+public class CompareNumbers {
+
+    private List<Integer> computerNumber;
+    private List<Integer> userNumber;
+
+    public List<Integer> getComputerNumber() {
+        return computerNumber;
+    }
+
+    public void setComputerNumber(List<Integer> computerNumber) {
+        this.computerNumber = computerNumber;
+    }
+
+    public List<Integer> getUserNumber() {
+        return userNumber;
+    }
+
+    public void setUserNumber(List<Integer> userNumber) {
+        this.userNumber = userNumber;
+    }
+}
--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -6,28 +6,29 @@ public class Game {
     private static final int NUMBER_SIZE = 3;
     private static final int NOTHING = 0;
 
-    public static int strike(List<Integer> computerNumber, List<Integer> userNumber) {
+    public static int strike(CompareNumbers compareNumbers) {
         int strikeCount = 0;
         for (int i = 0; i < NUMBER_SIZE; i++) {
-            if (computerNumber.get(i) == userNumber.get(i)) {
+            if (compareNumbers.getComputerNumber().get(i) == compareNumbers.getUserNumber().get(i)) {
                 strikeCount++;
             }
         }
         return strikeCount;
     }
 
-    public static int ball(List<Integer> computerNumber, List<Integer> userNumber) {
+    public static int ball(CompareNumbers compareNumbers) {
         int ballCount = 0;
         for (int i = 0; i < NUMBER_SIZE; i++) {
-            if (!(computerNumber.get(i) == userNumber.get(i)) && computerNumber.contains(userNumber.get(i))) {
+            if (!(compareNumbers.getComputerNumber().get(i) == compareNumbers.getUserNumber().get(i))
+                    && compareNumbers.getComputerNumber().contains(compareNumbers.getUserNumber().get(i))) {
                 ballCount++;
             }
         }
         return ballCount;
     }
 
-    public static boolean nothing(List<Integer> computerNumber, List<Integer> userNumber) {
-        if (ball(computerNumber, userNumber) == NOTHING && strike(computerNumber, userNumber) == NOTHING) {
+    public static boolean nothing(CompareNumbers compareNumbers) {
+        if (ball(compareNumbers) == NOTHING && strike(compareNumbers) == NOTHING) {
             return true;
         }
         return false;
