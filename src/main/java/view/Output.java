9a6f872ec438e45d0e9dbf2774d2dee6e004fec1
yumyeonghan <mgtmh991013@gmail.com>
2022-12-01 16:25:14 +0900

    feat: [Output Class], 결과 출력 기능 구현

---
+++ src/main/java/view/Output.java
@@ -0,0 +1,31 @@
+package view;
+
+import baseball.CompareNumbers;
+import baseball.Game;
+
+public class Output {
+
+    public void printResult(CompareNumbers compareNumbers) {
+        int ballCount = Game.ball(compareNumbers);
+        int strikeCount = Game.strike(compareNumbers);
+
+        System.out.println(ballCount + "볼 " + strikeCount + "스트라이크");
+
+        matchAllNumbers(strikeCount);
+
+        matchNoNumbers(compareNumbers);
+    }
+
+    private void matchNoNumbers(CompareNumbers compareNumbers) {
+        if (Game.nothing(compareNumbers)) {
+            System.out.println("낫싱");
+        }
+    }
+
+    private void matchAllNumbers(int strikeCount) {
+        if (strikeCount == 3) {
+            System.out.println("3스트라이크");
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        }
+    }
+}
