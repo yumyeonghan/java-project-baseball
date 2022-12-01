3f08acc8dd6b308864b3588f4a25228bf38e3952
yumyeonghan <mgtmh991013@gmail.com>
2022-12-01 09:52:39 +0900

    feat: [Computer Class], 컴퓨터 클래스 생성
    중복되지 않는 컴퓨터의 번호를 생성해주는 generateNumber() 구현

---
+++ src/main/java/baseball/Computer.java
@@ -0,0 +1,22 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Computer {
+
+    List<Integer> computerNumber;
+
+    public List<Integer> generateNumber() {
+        computerNumber = new ArrayList<>();
+        while (computerNumber.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computerNumber.contains(randomNumber)) {
+                computerNumber.add(randomNumber);
+            }
+        }
+        return computerNumber;
+    }
+}
