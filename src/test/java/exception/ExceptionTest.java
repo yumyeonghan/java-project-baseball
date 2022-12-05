cb5e849e8e1e7339e0e2ff60791b2b5073e1ce02
yumyeonghan <mgtmh991013@gmail.com>
2022-12-05 16:09:14 +0900

    test: 예외 상황 동작 테스트
    중복된 숫자 검증 테스트

---
+++ src/test/java/exception/ExceptionTest.java
@@ -0,0 +1,19 @@
+package exception;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class ExceptionTest {
+
+    @ValueSource(strings = {"1,2,2"})
+    @ParameterizedTest
+    void 중복된_숫자_검증_테스트(String inputNumbers) {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> Exception.validateNumbers(inputNumbers))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+}
