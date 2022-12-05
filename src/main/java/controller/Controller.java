0c327f29be6db50b838b9c68f9a89eca42a2ffb5
yumyeonghan <mgtmh991013@gmail.com>
2022-12-05 18:17:20 +0900

    refactor: 프로그래밍 요구사항에 맞게 리팩토링
    indent(인덴트, 들여쓰기) depth를 2까지만 허용함

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -14,21 +14,44 @@ public class Controller {
     private final Output output = new Output();
 
     public boolean run() {
-
-        CompareNumbers compareNumbers = new CompareNumbers();
-        Computer computer = new Computer();
-        compareNumbers.setComputerNumber(computer.generateNumber());
+        CompareNumbers compareNumbers = initCompareNumbers();
+        int inputNumber = 0; // 재시작,종료에 사용할 숫자 초기화
 
         while (true) {
             compareNumbers.setUserNumber(input.inputNumber());
             output.printResult(Game.gameResult(compareNumbers));
-            if (Game.strike(compareNumbers) == TERMINATION_CONDITION) {
-                if (input.restart() == 1) {
-                    return true;
-                }
-                break;
-            }
+
+            inputNumber = getNumberInput(compareNumbers, inputNumber);
+            if (checkRestartCondition(inputNumber)) return true;
+            if (checkExitCondition(inputNumber)) return false;
+        }
+    }
+
+    private CompareNumbers initCompareNumbers() {
+        CompareNumbers compareNumbers = new CompareNumbers();
+        Computer computer = new Computer();
+        compareNumbers.setComputerNumber(computer.generateNumber());
+        return compareNumbers;
+    }
+
+    private boolean checkExitCondition(int inputNumber) {
+        if (inputNumber == 2) {
+            return true;
         }
         return false;
+    }
+
+    private boolean checkRestartCondition(int inputNumber) {
+        if (inputNumber == 1) {
+            return true;
+        }
+        return false;
+    }
+
+    private int getNumberInput(CompareNumbers compareNumbers, int inputNumber) {
+        if (Game.strike(compareNumbers) == TERMINATION_CONDITION) {
+            inputNumber = input.restart();
+        }
+        return inputNumber;
     }
 }
