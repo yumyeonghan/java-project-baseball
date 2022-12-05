8df155e54b392d299055d68c194fdcdb27f5ff31
yumyeonghan <mgtmh991013@gmail.com>
2022-12-05 15:47:57 +0900

    feat: 입력 받은 숫자 검증하는 함수 추가
    중복된 숫자가 있는지 확인하는 기능

--- src/main/java/exception/Exception.java
+++ src/main/java/exception/Exception.java
@@ -1,9 +1,20 @@
 package exception;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class Exception {
 
     public static void ValidateNumbers(String inputNumbers) {
         checkNonNumericCharacters(inputNumbers);
+
+        checkDuplicateNumber(inputNumbers);
+    }
+
+    private static void checkDuplicateNumber(String inputNumbers) {
+        if (Arrays.stream(inputNumbers.split("")).collect(Collectors.toSet()).size() != inputNumbers.length()) {
+            throw new IllegalArgumentException();
+        }
     }
 
     private static void checkNonNumericCharacters(String inputNumbers) {
